e2d5c6b8fa807b80a8fdf4403f423aaa7527db5f
root <root@ip-172-31-28-122.us-west-1.compute.internal>
2023-10-04 08:07:53 +0000

    all files are commited

---
+++ amplify-appsync/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyappsync": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
